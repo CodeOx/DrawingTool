ccacbc632eaf2e85aebe37c7eaca26a762e919b0
Chetan Mittal <mittalchetan98@gmail.com>
2018-03-12 17:30:11 +0530

    completed PossibleSurfaces

--- PossibleSurfaces.cpp
+++ PossibleSurfaces.cpp
@@ -1,8 +1,24 @@
 #include "ThreeDModelGenerator.h"
 #include <vector>
 
+Point crossProduct(Point p1, Point p2){
+	Point p;
+	p.setX((p1.getY()*p2.getZ()) - (p1.getZ()*p2.getY()));
+	p.setY((p1.getX()*p2.getZ()) - (p1.getZ()*p2.getX()));
+	p.setZ((p1.getX()*p2.getY()) - (p1.getY()*p2.getX()));
+
+	return p;
+}
+
 bool checkEqualPoints(Point p1, Point p2){
 	if(p1.getX() == p2.getX() && p1.getY() == p2.getY() && p1.getZ() == p2.getZ()){
+		return true;
+	}
+	return false;
+}
+
+bool checkEqualLines(Line l1, Line l2){
+	if(checkEqualPoints(l1.getFirstPoint(), l2.getFirstPoint()) && checkEqualPoints(l1.getSecondPoint(), l2.getSecondPoint())){
 		return true;
 	}
 	return false;
@@ -22,7 +38,27 @@ bool checkIntersecting(Line l1, Line l2){
 }
 
 Plane getPlane(Line l1, Line l2){
+	Plane p;
 
+	Point p1 = l1.getFirstPoint();
+	Point p2 = l1.getSecondPoint();
+	Point p3;
+	if(checkEqualPoints(p1, l2.getFirstPoint())){
+		p3 = l2.getSecondPoint();
+	}
+	else{
+		p3 = l2.getFirstPoint();
+	}
+
+	Point p4 = crossProduct(p1,p2);
+	p.setA(p4.getX());
+	p.setB(p4.getY());
+	p.setC(p4.getZ());
+
+	float negD = (p4.getX()*p3.getX()) + (p4.getY()*p3.getY()) + (p4.getZ()*p3.getZ());
+	p.setD((-1)*negD);
+
+	return p;
 }
 
 bool checkEqualPlanes(Plane p1, Plane p2){
@@ -49,6 +85,15 @@ PlaneWithLines* planeWithLinesListContains(Plane p, PlaneWithLines* plwithln, in
 	return nullptr;
 }
 
+bool lineListContains(Line l, Line* lineList, int size){
+	for(int i = 0; i < size; i++){
+		if(checkEqualLines(l, *lineList)){
+			return true;
+		}
+	}
+	return false;
+}
+
 planeWithLinesList ThreeDModelGenerator::PossibleSurfacesConstructor(LineList possibleEdges){
 	Line* lines = possibleEdges.getlines();
 	int numLines = possibleEdges.getSize();
@@ -73,8 +118,10 @@ planeWithLinesList ThreeDModelGenerator::PossibleSurfacesConstructor(LineList po
 					list.push_back(newPlaneWithLines);
 				}
 				else{
-					(*originalPlane).addLine(*tempLines);
-					(*originalPlane).addLine(*lines);
+					if(!lineListContains(*tempLines, (*originalPlane).getArrayLines(), (*originalPlane).getNumLines()))
+						(*originalPlane).addLine(*tempLines);
+					if(!lineListContains(*lines, (*originalPlane).getArrayLines(), (*originalPlane).getNumLines()))
+						(*originalPlane).addLine(*lines);
 				}
 			
 			}
@@ -85,10 +132,6 @@ planeWithLinesList ThreeDModelGenerator::PossibleSurfacesConstructor(LineList po
 		
 		lines++;
 	}
-
-	/*
-		Loop to remove duplcate lines here
-	*/
 
 	planeWithLinesList arr;
 	arr.setPlaneWithLines(&list[0]);
