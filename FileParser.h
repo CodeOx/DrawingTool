1919268fba103c020efa4ce3cc0c94c20ca70abc
Ujjwal Gupta <ujjwalgupta1262@gmail.com>
2018-04-08 17:08:26 +0530

    Update FileParser.h

--- FileParser.h
+++ FileParser.h
@@ -1,15 +1,22 @@
 #include "model.h"
+#include "Lists.h"
 #include <string>
 
 #ifndef FILEPARSER_H
 #define FILEPARSER_H
 
+/*! \class FileParser
+A class representing a file parser  
+*/
 class FileParser{
 
 public:
+	//! function which takes a 3D model as input
 	ThreeDModel _3DModelInput(std::string filename);
-
+	//! function which takes a 2D model as input
 	TwoDModel _2DModelInput(std::string filename);
+
+	PointList getRemovePoints(std::string filename);
 
 	void parseFile(std::string filename, int choice);
 };
